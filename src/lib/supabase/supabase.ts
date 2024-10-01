e186ef255d13a42edfb83a2704d1fc6abd12584e
Vince77361 <vincelee@iyubiui-MacBookAir-6.local>
2024-10-01 17:13:42 +0900

    supabase deleted

--- src/lib/supabase/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://tzdpilddobrdgxrwlyzk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6ZHBpbGRkb2JyZGd4cndseXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0ODY2MDcsImV4cCI6MjA0MzA2MjYwN30.W5JkpG_UjBfRqd08QCIJdTpwR3Rpl5-MiA6fhj8vi0M";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import supabase from "./lib/supabase/supabase.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
-      <App />
-    </SessionContextProvider>
+    <App />
   </StrictMode>
 );
